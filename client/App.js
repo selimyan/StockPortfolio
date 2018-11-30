31e41e9a77eabe9addc0a4822558249e0f36b065
selimyan <gselimyan@gmail.com>
2018-11-30 17:54:23 -0500

    initial view checking user

--- client/App.js
+++ client/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react"
+import axios from 'axios'
 
 export default class App extends Component {
   constructor(props) {
-    super()
+    super(props)
     this.state = {
       user: {},
     }
   }
 
-  componentDidMount() {
-
+  async componentDidMount() {
+    try {
+      const res = await axios.get('/auth/me')
+      const user = res.data ? { ...res.data, isLoggedIn: true } : {}
+      this.setState({ user })
+    } catch (error) {
+      console.log('Error getting the user', error)
+    }
   }
 
   render() {
+    const { user } = this.state
+
     return (
       <div>
-        <h1>Hello</h1>
+        {
+          user.isLoggedIn ?
+            <h1>We need a navbar and user info here</h1>
+            : <h1>Let's sign you in</h1>
+        }
       </div>
     )
   }
