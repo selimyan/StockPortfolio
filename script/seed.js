e823040e14f10420f19f980b2b2b9f639dfd994b
selimyan <gselimyan@gmail.com>
2018-12-03 12:01:27 -0500

    seed file added

---
+++ script/seed.js
@@ -0,0 +1,43 @@
+const db = require('../server/db')
+const { User, Transaction } = require('../server/db/models')
+
+async function seed() {
+  await db.sync({ force: true })
+  console.log('---db synced---')
+
+  const users = await Promise.all([
+    User.create({ email: 'gaya@gmail.com', password: 'ppp', name: 'Gaya' }),
+    User.create({ email: 'dave@gmail.com', password: 'ppp', name: 'Dave' }),
+    User.create({ email: 'ofo@gmail.com', password: 'ppp', name: 'Ofo' })
+  ])
+
+  const transactions = await Promise.all([
+    Transaction.create({ ticker: 'aapl', quantity: 4, price: 30000, userId: 1 }),
+    Transaction.create({ ticker: 'stwd', quantity: 40, price: 2056, userId: 1 }),
+    Transaction.create({ ticker: 'aapl', quantity: 2, price: 28000, userId: 1 }),
+    Transaction.create({ ticker: 'nflx', quantity: 86, price: 10000, userId: 2 }),
+    Transaction.create({ ticker: 'msft', quantity: 10, price: 25000, userId: 2 }),
+    Transaction.create({ ticker: 'att', quantity: 2, price: 1800, userId: 3 }),
+  ])
+
+  console.log(`---seeded successfully ${users.length} users, ${transactions.length} transactions---`)
+
+}
+
+async function runSeed() {
+  try {
+    await seed()
+  } catch (err) {
+    console.error(err)
+  } finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
+
+if (module === require.main) {
+  runSeed()
+}
+
+module.exports = seed
