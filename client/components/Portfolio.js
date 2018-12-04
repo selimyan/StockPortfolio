696cdeb2f843041da07c3363e9f3cd214055d980
selimyan <gselimyan@gmail.com>
2018-12-04 12:22:04 -0500

    cleaned portfolio code

--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import TransactionForm from './TransactionForm'
-import { stringifyTickers } from './utils'
+import { stringifyTickers, getCurrentValue, createPortfolio } from './utils'
 
 export default class Portfolio extends Component {
   constructor(props) {
@@ -10,6 +10,7 @@ export default class Portfolio extends Component {
       user: {},
       value: 0,
       portfolio: [],
+      error: ''
     }
     this.buyShares = this.buyShares.bind(this)
   }
@@ -18,8 +19,8 @@ export default class Portfolio extends Component {
     try {
       const { user } = this.props
       const { data } = await axios.get('/api/portfolio')
-      const portfolio = await this.getCurrentInfo(data)
-      const value = this.getCurrentValue(portfolio)
+      const portfolio = await this.getCurrentPortfolio(data)
+      const value = getCurrentValue(portfolio)
       this.setState({
         user,
         portfolio,
@@ -30,40 +31,23 @@ export default class Portfolio extends Component {
     }
   }
 
-  async getCurrentInfo(stocks) {
+  async getCurrentPortfolio(stocks) {
     try {
-      let tickers = stringifyTickers(stocks)
+      const tickers = stringifyTickers(stocks)
       const { data } = await axios.get(`https://api.iextrading.com/1.0/stock/market/batch?symbols=${tickers}&types=quote`)
-      let portfolio = []
-      for (let i = 0; i < stocks.length; i++) {
-        let currentStock = stocks[i]
-        let item = {
-          ticker: currentStock.ticker,
-          quantity: currentStock.quantity,
-          price: data[currentStock.ticker].quote.latestPrice,
-          value: data[currentStock.ticker].quote.latestPrice * currentStock.quantity
-        }
-        portfolio.push(item)
-      }
-      return portfolio
+      return createPortfolio(stocks, data)
     } catch (error) {
       console.log(error)
     }
   }
 
-  getCurrentValue(portfolio) {
-    let total = 0
-    for (let i = 0; i < portfolio.length; i++) {
-      let stock = portfolio[i]
-      total += stock.value
-    }
-    return total
-  }
-
   async buyShares(purchaseInfo) {
-    console.log('purchase info', purchaseInfo)
     const { data } = await axios.post('/api/transactions', purchaseInfo)
     console.log('data', data)
+    if (data.error) this.setState({ error: data.error })
+    else {
+
+    }
   }
 
   render() {
--- client/components/utils.js
+++ client/components/utils.js
@@ -6,3 +6,26 @@ export const stringifyTickers = (portfolio) => {
   return tickers.join(',').toLowerCase()
 }
 
+export const getCurrentValue = (portfolio) => {
+  let total = 0
+  for (let i = 0; i < portfolio.length; i++) {
+    let stock = portfolio[i]
+    total += stock.value
+  }
+  return total
+}
+
+export const createPortfolio = (stocks, data) => {
+  let portfolio = []
+  for (let i = 0; i < stocks.length; i++) {
+    let currentStock = stocks[i]
+    let item = {
+      ticker: currentStock.ticker,
+      quantity: currentStock.quantity,
+      price: data[currentStock.ticker].quote.latestPrice,
+      value: data[currentStock.ticker].quote.latestPrice * currentStock.quantity
+    }
+    portfolio.push(item)
+  }
+  return portfolio
+}
