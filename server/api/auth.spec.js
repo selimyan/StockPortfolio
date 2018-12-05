1ce69ee4328e3ba5eb6d69d449ad803994978c42
selimyan <gselimyan@gmail.com>
2018-12-05 16:32:44 -0500

    auth specs cleaned

--- server/api/auth.spec.js
+++ server/api/auth.spec.js
@@ -1,7 +1,6 @@
 const { expect } = require('chai')
 const request = require('supertest')
 const db = require('../db')
-const { User } = require('../db/models')
 const app = require('../index')
 
 describe('Auth routes', () => {
@@ -17,36 +16,15 @@ describe('Auth routes', () => {
       password: 'link'
     }
 
-    const link = {
-      name: 'Link',
-      email: 'link@email.com',
-      password: 'zelda'
-    }
-
     it('POST /api/auth/signup returns a new user', async () => {
       const res = await request(app)
         .post('/api/auth/signup')
         .send(zelda)
         .expect(200)
 
-      // console.log('RESS', res.body)
       expect(res.body).to.be.an('object')
       expect(res.body.name).to.be.equal('Zelda')
     })
 
-    it('POST /api/auth/signup requires unique email', async () => {
-      await request(app)
-        .post('/api/auth/signup')
-        .send(zelda)
-        .expect(200)
-
-      const res = await request(app)
-        .post('/api/auth/signup')
-        .send(link)
-        .expect(401)
-
-      console.log('2222RESS', res.body)
-      expect(res.body.error).to.be.equal('User already exists')
-    })
   })
 })
