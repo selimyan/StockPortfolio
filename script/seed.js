90926c9a3aa606728543cceab1b19d860b9c00b7
selimyan <gselimyan@gmail.com>
2018-12-10 21:40:19 -0500

    new associations and seed file

--- script/seed.js
+++ script/seed.js
@@ -1,26 +1,34 @@
 const db = require('../server/db')
-const { User, Transaction } = require('../server/db/models')
+const { User, Transaction, Share } = require('../server/db/models')
 
 async function seed() {
   await db.sync({ force: true })
   console.log('---db synced---')
 
   const users = await Promise.all([
-    User.create({ email: 'gaya@gmail.com', password: 'ppp', name: 'Gaya' }),
-    User.create({ email: 'dave@gmail.com', password: 'ppp', name: 'Dave' }),
-    User.create({ email: 'ofo@gmail.com', password: 'ppp', name: 'Ofo' })
+    User.create({ email: 'gaya@gmail.com', password: 'ppp', name: 'Gaya', cash: 304052 }),
+    User.create({ email: 'dave@gmail.com', password: 'ppp', name: 'Dave', cash: 170928 }),
+    User.create({ email: 'ofo@gmail.com', password: 'ppp', name: 'Ofo', cash: 493854 })
   ])
 
   const transactions = await Promise.all([
     Transaction.create({ ticker: 'aapl', quantity: 4, price: 17618, userId: 1 }),
     Transaction.create({ ticker: 'stwd', quantity: 40, price: 2256, userId: 1 }),
     Transaction.create({ ticker: 'aapl', quantity: 2, price: 17618, userId: 1 }),
-    Transaction.create({ ticker: 'nflx', quantity: 86, price: 27509, userId: 2 }),
+    Transaction.create({ ticker: 'nflx', quantity: 8, price: 27509, userId: 2 }),
     Transaction.create({ ticker: 'msft', quantity: 10, price: 10900, userId: 2 }),
-    Transaction.create({ ticker: 'att', quantity: 2, price: 3073, userId: 3 }),
+    Transaction.create({ ticker: 't', quantity: 2, price: 3073, userId: 3 }),
   ])
 
-  console.log(`---seeded successfully ${users.length} users, ${transactions.length} transactions---`)
+  const shares = await Promise.all([
+    Share.create({ ticker: 'aapl', quantity: 6, userId: 1 }),
+    Share.create({ ticker: 'stwd', quantity: 40, userId: 1 }),
+    Share.create({ ticker: 'ntflx', quantity: 8, userId: 2 }),
+    Share.create({ ticker: 'msft', quantity: 10, userId: 2 }),
+    Share.create({ ticker: 't', quantity: 2, userId: 3 })
+  ])
+
+  console.log(`---seeded successfully ${users.length} users, ${transactions.length} transactions, ${shares.length} shares---`)
 
 }
 
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,10 +1,13 @@
 const User = require('./user')
 const Transaction = require('./transaction')
+const Share = require('./shares')
 
 //associations
 User.hasMany(Transaction)
+User.hasMany(Share)
 
 module.exports = {
   User,
-  Transaction
+  Transaction,
+  Share
 }
